b52ccb6998b333eefaf51275abb5277b287c9e94
jay870423 <695274107@qq.com>
2026-01-05 21:30:46 +0800

    修改bug

--- components/MobileControls.tsx
+++ components/MobileControls.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import { Crosshair, ArrowUpCircle } from 'lucide-react';
 import { InputState } from '../types';
 
